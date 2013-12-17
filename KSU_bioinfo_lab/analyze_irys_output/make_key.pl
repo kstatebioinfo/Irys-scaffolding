8a8607bf3d98058764c5bdff81db02179ec00bd3
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2013-12-17 17:58:31 -0600

    added output name argument

--- KSU_bioinfo_lab/analyze_irys_output/make_key.pl
+++ KSU_bioinfo_lab/analyze_irys_output/make_key.pl
@@ -5,11 +5,11 @@ use warnings;
 
 # STEP 1
 # this script outputs a cmap_key for the ./query_agp.pl script
-# USAGE: perl make_key.pl [fasta]
+# USAGE: perl make_key.pl [fasta] [output basename]
 
 open FASTA, '<', $ARGV[0] or die "Couldn't open $ARGV[0]: !";
 if (-e "$ARGV[0]"."_key") {print "$ARGV[0]_key file Exists!\n"; exit;}
-open KEY, '>', "$ARGV[0]"."_key" or die "Couldn't open $ARGV[0]_key: $!";
+open KEY, '>', "$ARGV[1]"."_key" or die "Couldn't open $ARGV[1]_key: !";
 print KEY "# Chromosome\tKnown(K) or Unknown (U)\tfastaHead\tfastaID\tcontigID\total length of contigs\tfastaStart\tfastaStop\n";
 
 my $counter=-1;
