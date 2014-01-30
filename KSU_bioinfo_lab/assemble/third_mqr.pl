1f25a3ac6f64bc5bafc265997a854a5f01edf5dd
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-01-29 19:02:38 -0600

    debugged third_mqr.pl

--- KSU_bioinfo_lab/assemble/third_mqr.pl
+++ KSU_bioinfo_lab/assemble/third_mqr.pl
@@ -36,12 +36,12 @@ push (@err_files,"${bnx_dir}/all_flowcells_adj_merged_bestref.err");
 ####################################################################
 my $third_mqr = `~/tools/RefAligner -i ${bnx_dir}/all_flowcells_adj_merged.bnx -o ${bnx_dir}/all_flowcells_adj_merged -T ${T} -nosplit 2 -M 5 -biaswt 0 -Mfast 0 -FP 1.5 -FN 0.15 -sf 0.2 -sd 0.2 -A 5 -S -1000 -res 3.5 -resSD 0.7 -outlier 1e-4 -endoutlier 1e-4 -minlen 150 -minsites 5`;
 print "$third_mqr";
-push (@err_files,"${bnx_dir}/all_flowcells_adj_merged.err")
+push (@err_files,"${bnx_dir}/all_flowcells_adj_merged.err");
 ####################################################################
 ##############  Compare  molecule quality reports         ##########
 ##############  with and without -BestRef                 ##########
 ####################################################################
-open (FLOWCELL_BNX_SUMMARY, '>', "$bnx_dir/bestref_effect_summary.csv") or die "can't open $bnx_dir/flowcell_summary.csv !\n"; # create file for summary stats of flowcell BNXs
+open (FLOWCELL_BNX_SUMMARY, '>', "$bnx_dir/bestref_effect_summary.csv") or die "can't open $bnx_dir/bestref_effect_summary.csv !\n"; # create file for summary stats of flowcell BNXs
 print FLOWCELL_BNX_SUMMARY "Filename,FP(/100kb),FNrate,bppSD,Maps,GoodMaps,GoodMaps/Maps\n";
 for my $file (@err_files)
 {
@@ -58,11 +58,11 @@ for my $file (@err_files)
             if ($values[7] != 0)
             {
                 my $map_ratio = $values[9]/$values[7];
-                print FLOWCELL_BNX_SUMMARY "${bnx_dir}/${filename}_adj_merged.bnx,$values[1],$values[2],$values[11],$values[7],$values[9],$map_ratio\n";
+                print FLOWCELL_BNX_SUMMARY "$file,$values[1],$values[2],$values[11],$values[7],$values[9],$map_ratio\n";
             }
             else
             {
-                print FLOWCELL_BNX_SUMMARY "${bnx_dir}/${filename}_adj_merged.bnx,bad flow cell 0 Maps\n";
+                print FLOWCELL_BNX_SUMMARY "$file,bad flow cell 0 Maps\n";
             }
         }
     }
