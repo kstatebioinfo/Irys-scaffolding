cb4bbd6a8182d8da30fb97c1bd7922f730fc7f19
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-01-31 16:06:58 -0600

    first pass parameter tests decided on in assemble.pl and debug merge_split_by_scan.pl

--- KSU_bioinfo_lab/assemble/assemble.pl
+++ KSU_bioinfo_lab/assemble/assemble.pl
@@ -25,6 +25,7 @@ my $err="${bnx_dir}/all_flowcells_adj_merged_bestref.err";
 my $T_relaxed = $T * 10;
 my $T_strict = $T/10;
 my pairmerge=75;
+my ($FP,$FN,$SiteSD_Kb,$ScalingSD_Kb_square);
 open (ERR,'<',"$err") or die "can't open $err!\n";
 while (<ERR>) # get noise parameters
 {
@@ -36,7 +37,7 @@ while (<ERR>) # get noise parameters
             s/\s+//g;
         }
         my $map_ratio = $values[9]/$values[7];
-        ($FP,$FN,SiteSD_Kb,ScalingSD_Kb_square)=($values[1],$values[2],$values[3],$values[4]);
+        ($FP,$FN,$SiteSD_Kb,$ScalingSD_Kb_square)=($values[1],$values[2],$values[3],$values[4]);
     }
 }
 
@@ -49,6 +50,47 @@ key1 => "$T_strict",
 key2 => 'value2',
 key3 => 'value3',
 );
+########################################
+##             Pairwise               ##
+########################################
+$ref->{pairwise}->{flag}->[0]->{val0} = $T
+########################################
+##               Noise                ##
+########################################
+$ref->{noise0}->{flag}->[0]->{val0} = $FP
+$ref->{noise0}->{flag}->[1]->{val0} = $FN
+$ref->{noise0}->{flag}->[2]->{val0} = $ScalingSD_Kb_square
+$ref->{noise0}->{flag}->[3]->{val0} = $SiteSD_Kb
+########################################
+##            Assembly                ##
+########################################
+$ref->{assembly}->{flag}->[0]->{val0} = $T
+########################################
+##              RefineA               ##
+########################################
+$ref->{refineA}->{flag}->[2]->{val0} = $T
+########################################
+##              RefineB               ##
+########################################
+$ref->{refineB}->{flag}->[2]->{val0} = $T/10
+$ref->{refineB}->{flag}->[9]->{val0} = 25; #min split length
+########################################
+##              RefineFinal           ##
+########################################
+$ref->{refineFinal}->{flag}->[2]->{val0} = $T/10
+$ref->{refineFinal}->{flag}->[16]->{val0} = 1e-5
+$ref->{refineFinal}->{flag}->[17]->{val0} = 1e-5
+########################################
+##              Extension             ##
+########################################
+$ref->{extension}->{flag}->[3]->{val0} = $T/10
+$ref->{extension}->{flag}->[20]->{val0} = 1e-5
+$ref->{extension}->{flag}->[20]->{val0} = 1e-5
+########################################
+##               Merge                ##
+########################################
+$ref->{merge}->{flag}->[0]->{val0} = 75
+$ref->{merge}->{flag}->[1]->{val0} = $T/1000
 
 for my $stringency (keys %p-value)
 {
