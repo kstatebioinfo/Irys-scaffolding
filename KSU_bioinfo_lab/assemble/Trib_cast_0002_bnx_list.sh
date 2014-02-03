24fcb95870e4fbcecb7db38adb10a43e108407ca
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-02-03 17:57:35 -0600

    prep data

---
+++ KSU_bioinfo_lab/assemble/Trib_cast_0002_bnx_list.sh
@@ -0,0 +1,98 @@
+!#/bin/bash
+mv /home/irys/Data/Datasets/A004\ 11518\ 8773\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 15_22/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules1.bnx 
+mv /home/irys/Data/Datasets/A004\ 11518\ 8773\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 15_41/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules2.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 14_28/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules3.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 14_42/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules4.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 14_56/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules5.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 15_10/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules6.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 15_37/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules7.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 15_48/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules8.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 16_02/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules9.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 16_16/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules10.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 16_29/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules11.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 16_47/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules12.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ 2013-03-18\ 17_56/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules13.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9084\ MchlPlg1\ dubleNLR\ DDaftNLR\ -\ NanoArray\ 1\ 2013-03-18\ 17_30/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules14.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 16_02/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules15.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 16_11/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules16.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 16_24/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules17.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 16_40/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules18.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 16_58/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules19.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 17_17/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules20.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 17_35/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules21.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 17_57/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules22.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 18_16/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules23.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 18_31/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules24.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 18_45/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules25.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 19_04/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules26.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9086\ Michelleplug1\ dbleNLR\ ddAFTnlr\ 2013-03-16\ 19_18/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules27.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9100\ MchlPlg2\ dblNLR\ 2013-03-19\ 16_39/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules28.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9100\ MchlPlg2\ dblNLR\ 2013-03-19\ 16_50/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules29.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9100\ MchlPlg2\ dblNLR\ 2013-03-19\ 17_20/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules30.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9100\ MchlPlg2\ dblNLR\ 2013-03-19\ 17_39/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules31.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9100\ MchlPlg2\ dblNLR\ 2013-03-19\ 18_34/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules32.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9100\ MchlPlg2\ dblNLR\ 2013-03-19\ 19_06/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules33.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9108\ Mclleplug1\ dubleNLR\ DDaftNLR\ 2013-03-16\ 20_09/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules34.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9108\ Mclleplug1\ dubleNLR\ DDaftNLR\ 2013-03-16\ 20_29/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules35.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9108\ Mclleplug1\ dubleNLR\ DDaftNLR\ 2013-03-16\ 20_59_2/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules36.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9108\ Mclleplug1\ dubleNLR\ DDaftNLR\ 2013-03-16\ 20_59/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules37.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MchlPlg1\ dblNLR\ DDaftrNLR\ 2013-03-19\ 12_37/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules38.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MchlPlg1\ dblNLR\ DDaftrNLR\ 2013-03-19\ 13_10/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules39.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MchlPlg1\ dblNLR\ DDaftrNLR\ 2013-03-19\ 13_31/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules40.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MchlPlg1\ dblNLR\ DDaftrNLR\ 2013-03-19\ 13_56/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules41.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MchlPlg1\ dblNLR\ DDaftrNLR\ 2013-03-19\ 14_11/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules42.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MchlPlg1\ dblNLR\ DDaftrNLR\ 2013-03-19\ 14_57/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules43.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MchlPlg1\ dblNLR\ DDaftrNLR\ 2013-03-19\ 15_16/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules44.bnx 
+mv /home/irys/Data/Datasets/A004\ 11539\ 9109\ MSCarlyprep\ dblNLR\ DDaftrNLR\ 2013-03-19\ 15_34/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules45.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_A004_Plug1_4-3_A32\ -\ NanoArray\ 1\ 2013-06-09\ 16_43/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules46.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_A004_Plug1_4-3_A32\ -\ NanoArray\ 2\ 2013-06-10\ 00_38/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules47.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_A007_Plug1_3-2_A32\ -\ NanoArray\ 1\ 2013-06-09\ 16_55/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules48.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_AU08_Plug2_3-2_A32\ 2013-06-11\ 14_30/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules49.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_AU08_Plug2_3-2_A32_Chip2_FC3\ 2013-06-11\ 14_36/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules50.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_AU08_Plug2_3-2_A32_Chip2\ -\ NanoArray\ 1\ 2013-06-11\ 21_24/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules51.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_AU08_Plug2_3-2_A32_Chip2\ -\ NanoArray\ 2\ 2013-06-12\ 03_37/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules52.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_AU08_Plug2_3-2_A32_FC1\ 2013-06-10\ 16_56/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules53.bnx 
+mv /home/irys/Data/Datasets/KSU_Trib_AU08_Plug2_3-2_A32_FC2\ 2013-06-10\ 21_40/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules54.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 1\ 2013-05-21\ 09_06/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules55.bnx 
+mv /home/irys/Data/Datasets/trib_cast_0002\ 1\ 2013-05-23\ 09_54/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules56.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 1\ 2013-05-24\ 12_17/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules57.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 1\ 2013-06-20\ 13_46/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules58.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 1\ 2013-06-21\ 17_20/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules59.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 1\ 2013-12-10\ 11_50/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules60.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 1\ 2013-12-11\ 10_37_2/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules61.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 1\ 2013-12-12\ 09_53/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules62.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_1_2013-12-16_13_58/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules63.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_1_2013-12-17_12_00/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules64.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_1_2013-12-30_10_02/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules65.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_1_2013-12-30_15_56/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules66.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_1_2013-12-31_09_01/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules67.bnx 
+mv /home/irys/Data/Datasets/Trib-cast\ 0002\ 2013-05-13\ 12_14/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules68.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-05-21\ 14_36/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules69.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ 2\ 2013-05-22\ 12_27/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules70.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-05-23\ 12_38/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules71.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-05-23\ 17_26/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules72.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-05-24\ 15_18/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules73.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-06-20\ 15_32/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules74.bnx 
+mv /home/irys/Data/Datasets/trib_cast_0002\ 2\ 2013-08-15\ 16_21/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules75.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-12-10\ 13_01/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules76.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-12-11\ 11_48_2/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules77.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ 2013-12-12\ 11_04/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules78.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_2_2014-01-02_11_09/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules79.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_2_2014-01-02_17_59/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules80.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_2_2014-01-03_10_01/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules81.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_2_2014-01-03_14_03/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules82.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002_2_2014-01-03_20_10/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules83.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ michelle\ 2013-09-18\ 15_25/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules84.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 2\ Michelle\ 2013-09-18\ 20_51/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules85.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 3\ 2013-05-21\ 16_05/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules86.bnx 
+mv /home/irys/Data/Datasets/Trib_cast_0002\ 3\ andrew\ 2013-09-19\ 03_05/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules87.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ ga-2\ prep\ with\ kit\ tube\ 1\ dual\ nick\ 2013-05-09\ 12_58/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules88.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ nlr\ 5-10-2013\ 2013-05-13\ 09_32/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules89.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ nlr\ 5-10-2013\ 2013-05-13\ 14_59/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules90.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ nlr\ 5-10-2013\ 2013-05-14\ 09_02/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules91.bnx 
+mv /home/irys/Data/Datasets/Trib_cast0002_NLRdate514\ 2013-05-14\ 12_06/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules92.bnx 
+mv /home/irys/Data/Datasets/trib_cast\ 0002\ prep\ from\ kit\ tube\ 1\ 2013-05-09\ 09_48/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules93.bnx 
+mv /home/irys/Data/Datasets/trib_cast\ 0002\ prep\ from\ kit\ tube\ 1\ 2013-05-09\ 16_36/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules94.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ prepped\ using\ kit\ tube\ 1\ dual\ nick\ 2013-05-10\ 12_31/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules95.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ prepped\ with\ kit\ Tube\ 1\ dual\ nick\ 2013-05-10\ 09_21/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules96.bnx 
+mv /home/irys/Data/Datasets/Trib_cast\ 0002\ prepped\ with\ kit\ tube\ 1\ dual\ nick\ 2013-05-10\ 15_10/Detect\ Molecules/RawMolecules.bnx /home/irys/Data/Trib_cast_0002/RawMolecules97.bnx 
