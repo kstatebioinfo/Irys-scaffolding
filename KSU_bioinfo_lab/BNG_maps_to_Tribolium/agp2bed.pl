0ca815b30683b5b1d0f7a2af6b5d46e87a3ba817
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-02-19 19:11:13 -0600

    update agp2bed.pl

--- KSU_bioinfo_lab/BNG_maps_to_Tribolium/agp2bed.pl
+++ KSU_bioinfo_lab/BNG_maps_to_Tribolium/agp2bed.pl
@@ -18,11 +18,25 @@ my $agp= $ARGV[1];
 my $scaffold = 1;
 open (FASTA,'<',$fasta) or die "can't open $fasta\n";
 open (AGP,'<',$agp) or die "can't open $agp\n";
+$fasta ~= /(.*)\.agp/$1\.bed/;
+open (BED,'>',"$fasta") or die "can't open $fasta\n";
 while (<FASTA>)
 {
     if (/^>/)
     {
         />Scaffold[0]*(.*)\s\|/;
-        
+        while (<AGP>)
+        {
+            unless (/^#/)
+            {
+                my @rows = split(\t);
+                if (($rows[4] eq "W")&&("Scaffold$1" eq "$rows[0]"))
+                {
+                    print BED "$count\t$rows[1]\t$rows[2]\t$rows[5]\n";
+                }
+            }
+        }
+        ++$count;
     }
+    
 }
