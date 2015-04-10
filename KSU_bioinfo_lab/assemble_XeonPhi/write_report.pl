9e814ec0b8fa68fb299ffe814185580d9c29373f
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2015-04-10 04:15:18 -0700

    finished up to in silico CMAP directory in KSU_bioinfo_lab/assemble_XeonPhi/write_report.pl

--- KSU_bioinfo_lab/assemble_XeonPhi/write_report.pl
+++ KSU_bioinfo_lab/assemble_XeonPhi/write_report.pl
@@ -3,7 +3,7 @@
 #
 #	USAGE: perl write_report.pl
 #
-#  Created by Jennifer Shelton 2/26/15
+#  Created by Jennifer Shelton 4/09/15
 #
 # DESCRIPTION: # Copy script into assembly working directory and update other variables for project in "Project variables" section. To do this cd to the assembly working directory and "cp ~/Irys-scaffolding/KSU_bioinfo_lab/assemble_XeonPhi/write_report.pl ." and then edit the new version to point to the best asssembly or the best assembly CMAP and the best alignment (Default or Relaxed).
 # REQUIREMENTS: Requires BNGCompare from https://github.com/i5K-KINBRE-script-share/BNGCompare in your home directory. Also requires BioPerl.
@@ -47,13 +47,13 @@ my $optional_assembled_cmap = ''; # add assembled cmap path here if it is not in
 my $genome_map_cmap;
 unless($optional_assembled_cmap)
 {
-    $genome_map_cmap = `ls ${best_dir}/contigs/*_refineFinal1/*_REFINEFINAL1.cmap`;
-    chomp($genome_map_cmap);
+    $genome_map_cmap = glob "${best_dir}/contigs/*_refineFinal1/*_REFINEFINAL1.cmap";
 }
 else
 {
     $genome_map_cmap = $optional_assembled_cmap;
 }
+my (${genome_map_filename}, ${genome_map_directories}, ${genome_map_suffix}) = fileparse($genome_map_cmap,qr/\.[^.]*/); # directories has trailing slash includes dot in suffix
 ###########################################################
 #          Sanity check project variables section
 ###########################################################
@@ -65,19 +65,44 @@ unless($enzyme =~ /(BspQI|BbvCI|BsrDI|bseCI)/)
 {
     die "Enzymes listed in the \"Project variables\" section of write_report.pl are not valid. Valid entries would be a space separated list that includes one or more of the following options \"BspQI BbvCI BsrDI bseCI\". Remember to copy the write_report.pl script into your assembly working directory and update other variables for project in \"Project variables\" section. To do this cd to the assembly working directory and \"cp ~/Irys-scaffolding/KSU_bioinfo_lab/assemble_XeonPhi/write_report.pl .\" and then edit the new version to point to the best asssembly or the best assembly CMAP and the best alignment (Default or Relaxed).\n";
 }
-
+###########################################################
+#          Check that report directory already exists
+###########################################################
 my $report_dir = "$best_dir/../$project";
-
+unless (-d "$report_dir")
+{
+    die "Exiting because $report_dir does not exist\n";
+}
+###########################################################
+#          Prepare: BioNano_consensus_cmap
+###########################################################
 mkdir "$report_dir/BioNano_consensus_cmap";
 unless (-d "$report_dir/BioNano_consensus_cmap")
 {
     die "Can't create $report_dir/BioNano_consensus_cmap/";
 }
+link ($genome_map_cmap, "$report_dir/BioNano_consensus_cmap/${genome_map_filename}.cmap") or warn "Can't link $genome_map_cmap to $report_dir/BioNano_consensus_cmap/${genome_map_filename}.cmap: $!";
+###########################################################
+#          Prepare: in_silico_cmap
+###########################################################
 mkdir "$report_dir/in_silico_cmap";
 unless (-d "$report_dir/in_silico_cmap")
 {
     die "Can't create $report_dir/in_silico_cmap/";
 }
+my (${cmap_filename}, ${cmap_directories}, ${cmap_suffix}) = fileparse($cmap,qr/\.[^.]*/); # directories has trailing slash includes dot in suffix
+link ($cmap,"$report_dir/in_silico_cmap/${cmap_filename}.cmap" ) or die "Can't link $cmap to $report_dir/in_silico_cmap/${cmap_filename}.cmap: $!";
+if (-f "${cmap_directories}${cmap_filename}_key.txt")
+{
+    link ("${cmap_directories}${cmap_filename}_key.txt","$report_dir/in_silico_cmap/${cmap_filename}_key.txt" or die "Can't link ${cmap_directories}${cmap_filename}_key.txt to $report_dir/in_silico_cmap/${cmap_filename}_key.txt: $!";
+}
+else
+{
+    die "Error you are missing a key to your in silico reference CMAP in the same directory as $cmap. Create a new CMAP with ~/bin/fa2cmap_multi.pl and try again.\n";
+}
+###########################################################
+#          Prepare: align_in_silico_xmap
+###########################################################
 mkdir "$report_dir/align_in_silico_xmap";
 unless (-d "$report_dir/align_in_silico_xmap")
 {
@@ -93,3 +118,6 @@ unless (-d "$report_dir/align_in_silico_super_scaffold_xmap")
 {
     die "Can't create $report_dir/align_in_silico_super_scaffold_xmap/";
 }
+my $compress = `cd ${best_dir}; tar -czvf ${project}.tar.gz $project`;
+print $compress;
+
